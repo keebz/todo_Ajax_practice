4afae53b23d81f0847a212e53d4a7f7fbcc5ed86
lee kebler <student@epicodus.com>
2014-10-06 12:06:07 -0700

    add form partial and render for creating tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,20 @@
+class TasksController < ApplicationController
+  before_filter :authenticate_user!, except: [:index]
+
+  def index
+    @tasks = Task.all
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to root_path
+  end
+
+
+  private
+  def task_params
+    params.require(:task).permit(:name)
+  end
+
+end
